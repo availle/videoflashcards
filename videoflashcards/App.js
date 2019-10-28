7d186831ef2024c2d564132b8057d245e593fc26
Stefanie Hasler <stefanie.hasler@codecentric.de>
2019-10-28 16:26:17 +0100

    First working app

--- videoflashcards/App.js
+++ videoflashcards/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const startFlashCarding = () => {
+  console.log('flashcarding started')
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a test.</Text>
+      <Button title="Start Flashcarding!" onPress={startFlashCarding}>Start Flash Card Training!</Button>
     </View>
   );
 }
@@ -12,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00bbbb',
     alignItems: 'center',
     justifyContent: 'center',
   },
